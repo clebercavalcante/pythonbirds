fef84926a11efe10273ba886452f6c880c890808
CleberCavalcante <cleber_caval@hotmail.com>
2020-07-20 22:52:39 -0300

    Criação do atributo complexo filho

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,13 +1,17 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=None):
+    def __init__(self, *filhos, nome=None, idade=None):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return 'Olá Mundo'
 
 if __name__ == '__main__':
-    p = Pessoa('Cleber', 30)
-    print(p.cumprimentar())
-    print(p.nome, 'tem', p.idade,'anos')
+    filho = Pessoa(nome='Enzo',idade= 8)
+    pai = Pessoa(filho, nome='Cleber', idade=30)
+    print(pai.cumprimentar())
+    print(pai.nome, 'tem', pai.idade,'anos')
+    for lista_filho in pai.filhos:
+        print('O filho de', pai.nome, 'se chama', lista_filho.nome, 'e tem', lista_filho.idade, 'anos de idade')
 
