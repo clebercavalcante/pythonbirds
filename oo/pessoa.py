813236fc4850e2e20ad7a867ea88e2a102500ca9
CleberCavalcante <cleber_caval@hotmail.com>
2020-07-20 22:40:49 -0300

    Criação dos atributos nome e idade na classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,13 @@
 class Pessoa:
-    pass
+    def __init__(self, nome=None, idade=None):
+        self.nome = nome
+        self.idade = idade
+
+    def cumprimentar(self):
+        return 'Olá Mundo'
+
+if __name__ == '__main__':
+    p = Pessoa('Cleber', 30)
+    print(p.cumprimentar())
+    print(p.nome, 'tem', p.idade,'anos')
+
